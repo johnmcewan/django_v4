d40afb5ce804cc584db0325528e4bc4caae9584d
John McEwan <jmcewan3@yahoo.ca>
2025-05-16 11:50:46 -0500

    updates

--- sealquery/settings.py
+++ sealquery/settings.py
@@ -107,8 +107,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'digital.sigillography.resource@gmail.com'
-EMAIL_HOST_PASSWORD = 'gges rnvg feii zgmh'
-#EMAIL_HOST_PASSWORD = os.environ.get('email')
+EMAIL_HOST_PASSWORD = os.environ.get('email')
 DEFAULT_FROM_EMAIL = 'digital.sigillography.resource@gmail.com'
 
 
